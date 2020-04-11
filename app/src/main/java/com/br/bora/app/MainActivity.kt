9ac0a376c2532d8ab2f2f8ced243dd216a56884c
GBernardo10 <g_bernardo10@hotmail.com>
2020-04-11 03:42:52 -0300

    feat: class login

--- app/src/main/java/com/br/bora/app/MainActivity.kt
+++ app/src/main/java/com/br/bora/app/MainActivity.kt
@@ -2,6 +2,7 @@ package com.br.bora.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
