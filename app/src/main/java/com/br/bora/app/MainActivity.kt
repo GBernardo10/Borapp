3547cc5b7743d3a6bd004dd39bd787cb745c0634
Guilherme Jesus <g_bernardo10@hotmail.com>
2020-04-14 21:56:11 -0300

    feat: add page home

--- app/src/main/java/com/br/bora/app/MainActivity.kt
+++ app/src/main/java/com/br/bora/app/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.br.bora.app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun irHome(v:View){
+        val telaHome = Intent(this,HomeActivity::class.java)
+        startActivity(telaHome)
     }
 }
