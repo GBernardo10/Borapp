3bd038a27effcf3882d4a791798adcc04f05cd80
bruno.barruda <bruno.barruda@tivit.com>
2020-05-04 03:12:47 -0300

    set onchange

--- app/src/main/java/com/br/bora/app/ConfiguracaoPreferenciaActivity.kt
+++ app/src/main/java/com/br/bora/app/ConfiguracaoPreferenciaActivity.kt
@@ -1,6 +1,7 @@
 package com.br.bora.app
 
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import android.graphics.drawable.GradientDrawable
 import androidx.appcompat.app.AppCompatActivity
@@ -34,29 +35,49 @@ class ConfiguracaoPreferenciaActivity : AppCompatActivity() {
         val bares = preferencias?.getBoolean("bares", this.baresSelected)
         val ioga = preferencias?.getBoolean("ioga", this.iogaSelected)
         val shows = preferencias?.getBoolean("shows", this.showsSelected)
+
+        if (futebol == true) {
+            card1.setPadding(3, 3, 3, 3)
+        }
+        if (rpg == true) {
+            card2.setPadding(3, 3, 3, 3)
+        }
+        if (festas == true) {
+            card3.setPadding(3, 3, 3, 3)
+        }
+        if (bares == true) {
+            card4.setPadding(3, 3, 3, 3)
+        }
+        if (ioga == true) {
+            card5.setPadding(3, 3, 3, 3)
+        }
+        if (shows == true) {
+            card6.setPadding(3, 3, 3, 3)
+        }
     }
 
     fun onChangeFutebol(v: View) {
         if (futebolSelected == false) {
             this.futebolSelected = true
-            editor?.putBoolean("futebol", this.futebolSelected)
+            editor?.putBoolean("futebol", true)
             card1.setPadding(3, 3, 3, 3)
         }else {
             this.futebolSelected = false
-            editor?.putBoolean("futebol", this.futebolSelected)
+            editor?.putBoolean("futebol", false)
             card1.setPadding(0, 0,0, 0)
         }
         editor?.commit()
+        editor?.apply()
     }
 
     fun onChangeRpg(v: View) {
         if (rpgSelected == false) {
             this.rpgSelected = true
-            editor?.putBoolean("rpg", this.rpgSelected)
+            editor?.putBoolean("rpg", true)
             card2.setPadding(3, 3, 3, 3)
         }else {
             this.rpgSelected = false
-            editor?.putBoolean("rpg", this.rpgSelected)
+            editor?.putBoolean("rpg", false)
             card2.setPadding(0, 0, 0, 0)
         }
         editor?.commit()
@@ -65,11 +86,11 @@ class ConfiguracaoPreferenciaActivity : AppCompatActivity() {
     fun onChangeFestas(v: View) {
         if (festasSelected == false) {
             this.festasSelected = true
-            editor?.putBoolean("festas", this.festasSelected)
+            editor?.putBoolean("festas", true)
             card3.setPadding(3, 3, 3, 3)
         }else {
             this.festasSelected = false
-            editor?.putBoolean("festas", this.festasSelected)
+            editor?.putBoolean("festas", false)
             card3.setPadding(0,0,0,0)
         }
         editor?.commit()
@@ -78,11 +99,11 @@ class ConfiguracaoPreferenciaActivity : AppCompatActivity() {
     fun onChangeBares(v: View) {
         if (baresSelected == false) {
             this.baresSelected = true
-            editor?.putBoolean("bares", this.baresSelected)
+            editor?.putBoolean("bares", true)
             card4.setPadding(3, 3, 3, 3)
         }else {
             this.baresSelected = false
-            editor?.putBoolean("bares", this.baresSelected)
+            editor?.putBoolean("bares", false)
             card4.setPadding(0, 0, 0, 0)
         }
         editor?.commit()
@@ -91,11 +112,11 @@ class ConfiguracaoPreferenciaActivity : AppCompatActivity() {
     fun onChangeIoga(v: View) {
         if (iogaSelected == false) {
             this.iogaSelected = true
-            editor?.putBoolean("ioga", this.iogaSelected)
+            editor?.putBoolean("ioga", true)
             card5.setPadding(3, 3, 3, 3)
         }else {
             this.iogaSelected = false
-            editor?.putBoolean("ioga", this.iogaSelected)
+            editor?.putBoolean("ioga", false)
             card5.setPadding(0, 0, 0, 0)
         }
         editor?.commit()
@@ -104,13 +125,22 @@ class ConfiguracaoPreferenciaActivity : AppCompatActivity() {
     fun onChangeShows(v: View) {
         if (showsSelected == false) {
             this.showsSelected = true
-            editor?.putBoolean("shows", this.iogaSelected)
+            editor?.putBoolean("shows", true)
             card6.setPadding(3, 3, 3, 3)
         }else {
             this.showsSelected = false
-            editor?.putBoolean("shows", this.iogaSelected)
+            editor?.putBoolean("shows", false)
             card6.setPadding(0, 0, 0, 0)
         }
+        editor?.commit()
+    }
+
+    fun irConfiguracao(v: View) {
+        val telaConfiguracoes = Intent(this, ConfiguracoesActivity::class.java);
+        startActivity(telaConfiguracoes);
+    }
+
+    fun salvar(v: View) {
         editor?.commit()
     }
 }
