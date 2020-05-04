3573a7a4a9180c8c8e1bc124f968d16b8c5beaf2
bruno.barruda <bruno.barruda@tivit.com>
2020-05-03 23:09:22 -0300

    set padding

--- app/src/main/java/com/br/bora/app/ConfiguracaoPreferenciaActivity.kt
+++ app/src/main/java/com/br/bora/app/ConfiguracaoPreferenciaActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.core.view.setPadding
 import kotlinx.android.synthetic.main.activity_configuracao_preferencia.*
 import kotlinx.android.synthetic.main.activity_confirmar_evento.view.*
 
@@ -39,10 +40,11 @@ class ConfiguracaoPreferenciaActivity : AppCompatActivity() {
         if (futebolSelected == false) {
             this.futebolSelected = true
             editor?.putBoolean("futebol", this.futebolSelected)
-
+            card1.setPadding(3, 3, 3, 3)
         }else {
             this.futebolSelected = false
             editor?.putBoolean("futebol", this.futebolSelected)
+            card1.setPadding(0, 0,0, 0)
         }
         editor?.commit()
     }
@@ -51,9 +53,11 @@ class ConfiguracaoPreferenciaActivity : AppCompatActivity() {
         if (rpgSelected == false) {
             this.rpgSelected = true
             editor?.putBoolean("rpg", this.rpgSelected)
+            card2.setPadding(3, 3, 3, 3)
         }else {
             this.rpgSelected = false
             editor?.putBoolean("rpg", this.rpgSelected)
+            card2.setPadding(0, 0, 0, 0)
         }
         editor?.commit()
     }
@@ -62,9 +66,11 @@ class ConfiguracaoPreferenciaActivity : AppCompatActivity() {
         if (festasSelected == false) {
             this.festasSelected = true
             editor?.putBoolean("festas", this.festasSelected)
+            card3.setPadding(3, 3, 3, 3)
         }else {
             this.festasSelected = false
             editor?.putBoolean("festas", this.festasSelected)
+            card3.setPadding(0,0,0,0)
         }
         editor?.commit()
     }
@@ -73,9 +79,11 @@ class ConfiguracaoPreferenciaActivity : AppCompatActivity() {
         if (baresSelected == false) {
             this.baresSelected = true
             editor?.putBoolean("bares", this.baresSelected)
+            card4.setPadding(3, 3, 3, 3)
         }else {
             this.baresSelected = false
             editor?.putBoolean("bares", this.baresSelected)
+            card4.setPadding(0, 0, 0, 0)
         }
         editor?.commit()
     }
@@ -84,9 +92,11 @@ class ConfiguracaoPreferenciaActivity : AppCompatActivity() {
         if (iogaSelected == false) {
             this.iogaSelected = true
             editor?.putBoolean("ioga", this.iogaSelected)
+            card5.setPadding(3, 3, 3, 3)
         }else {
             this.iogaSelected = false
             editor?.putBoolean("ioga", this.iogaSelected)
+            card5.setPadding(0, 0, 0, 0)
         }
         editor?.commit()
     }
@@ -95,9 +105,11 @@ class ConfiguracaoPreferenciaActivity : AppCompatActivity() {
         if (showsSelected == false) {
             this.showsSelected = true
             editor?.putBoolean("shows", this.iogaSelected)
+            card6.setPadding(3, 3, 3, 3)
         }else {
             this.showsSelected = false
             editor?.putBoolean("shows", this.iogaSelected)
+            card6.setPadding(0, 0, 0, 0)
         }
         editor?.commit()
     }
