29b3f4bcfe9314644ad75a2c61be908d5068eb18
bruno.barruda <bruno.barruda@tivit.com>
2020-05-03 21:45:25 -0300

    image view

--- app/src/main/java/com/br/bora/app/ConfiguracoesActivity.kt
+++ app/src/main/java/com/br/bora/app/ConfiguracoesActivity.kt
@@ -1,12 +1,29 @@
 package com.br.bora.app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class ConfiguracoesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_configuracoes)
+    }
+
+    fun irEditarPerfil(v: View) {
+        val telaEditarPerfil = Intent(this,CadastroPfActivity::class.java)
+        startActivity(telaEditarPerfil)
+    }
+
+    fun irConfiguracoesPreferenciais(v: View) {
+        val telaConfiguracoesPreferenciais = Intent(this,ConfiguracaoPreferenciaActivity::class.java)
+        startActivity(telaConfiguracoesPreferenciais)
+    }
+
+    fun logout(v: View) {
+        val telaLogin = Intent(this,MainActivity::class.java)
+        startActivity(telaLogin)
     }
 }
