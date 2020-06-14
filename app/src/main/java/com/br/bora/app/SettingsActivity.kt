3c117fed559a0ea0ae02585a8b08b5764f8880ad
GBernardo10 <g_bernardo10@hotmail.com>
2020-06-14 04:40:38 -0300

    chore: remove fields in settings, change name for settings, both in xml class kt

--- app/src/main/java/com/br/bora/app/SettingsActivity.kt
+++ app/src/main/java/com/br/bora/app/SettingsActivity.kt
@@ -3,27 +3,33 @@ package com.br.bora.app
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import kotlinx.android.synthetic.main.activity_configuracoes.*
+import androidx.appcompat.app.AppCompatDelegate
+import com.br.bora.app.utils.SaveData
+import kotlinx.android.synthetic.main.activity_settings.*
 
-class ConfiguracoesActivity : AppCompatActivity() {
+class SettingsActivity : AppCompatActivity() {
+    private lateinit var saveData: SaveData
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_configuracoes)
+        setContentView(R.layout.activity_settings)
+        saveData = SaveData(this)
+        switch_theme.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                saveData.setDarkModeState(true)
 
-        btEditarPerfil.setOnClickListener {
-            startActivity(Intent(this,EditarUsuarioActivity::class.java))
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                saveData.setDarkModeState(false)
+            }
         }
-        btEditarSenha.setOnClickListener {
-            startActivity(Intent(this,SalvarNovaSenha::class.java))
+
+        btn_edit_password_settings.setOnClickListener {
+            startActivity(Intent(this, SalvarNovaSenha::class.java))
         }
         btPreferencias.setOnClickListener {
-            startActivity(Intent(this,ConfiguracaoPreferenciaActivity::class.java))
-        }
-        btSair.setOnClickListener {
-            //Limpar o preferences com o nome do usuário
-            startActivity(Intent(this,MainActivity::class.java))
+            startActivity(Intent(this, ConfiguracaoPreferenciaActivity::class.java))
         }
     }
 }
